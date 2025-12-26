d93739878d295ed1cb2deb57f2680538396d3a36
Prof. Sarah Lin <slin@pace.edu>
2025-12-26 10:44:07 -0500

    Create Lab01.java

---
+++ Lab01.java
@@ -0,0 +1,10 @@
+public class Lab01 {
+    public static void main(String[] args) {
+        // Task 2: Write code to print "Hello, World!"
+        
+        // Task 3: Write code to print "Professor", "Professor Sarah", and "Professor Lin"
+        
+        // Task 4: Write code to print a right triangle of asterisks (*)
+        
+    }
+}
